106bd729dcf74d39588ad7a4889cda70865f70a8
Martin Grimme <martin.grimme@gmail.com>
2023-06-26 18:00:56 +0200

    updated

--- shellfish/ui-gallery/tiles/water.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="water" tilewidth="32" tileheight="32" tilecount="216" columns="18">
- <image source="wateranimate2.png" width="576" height="386"/>
-</tileset>
